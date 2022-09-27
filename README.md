f6d488ef803409a7875537d8c48bd8ba5c0e4e91
Atanael Cardoso <110429151+atanaelcardoso@users.noreply.github.com>
2022-09-27 18:14:07 +0000

    Concluindo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # criando-banco-de-dados
+
+ api de banco de dados ele tem :
+ .sqlite: software de banco de dados.
+ dentro de dele tem um estatus de seguranca que sao cors, https ,fs.
---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import fs from 'fs';
+import https from 'https';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+import router from './routes.js'
+app.use(router);
+
+// insonia app que ele usa
+// cors forma de seguransa , npm i cors -s, npm i https -s,npm i fs -s
+//estatus de seguranca 'https://pt.rakko.tools/tools/46/' dendro dele ele fez as pasta cors, https ,fs
+app.listen(3000, () => console.log("Api Rodando."))
+
+https.createServer({
+  cert: fs.readFileSync('src/SSL/code.crt'),
+  key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(3001, () => console.log("Rodando em https"));
