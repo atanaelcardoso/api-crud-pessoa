46889d5f866a508e91df050edeb16f92a84e4814
Jonathan Cardoso <jonathan99mour@gmail.com>
2022-10-15 20:32:49 -0300

    Fix: Corrigindo parametros e uso de async/await nas chamadas de banco de dados

--- src/repositories/people-repository.ts
+++ src/repositories/people-repository.ts
@@ -3,52 +3,58 @@ import { openDb } from '../configDB';
 
 export async function createTable() {
   openDb().then(db => {
-    db.exec('CREATE TABLE IF NOT EXISTS Pessoa ( id INTEGER PRIMARY KEY, nome TEXT, idate INTEGER)');
+    db.exec('CREATE TABLE IF NOT EXISTS Pessoa ( id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER)');
   });
 };
 
-export async function selectPessoas(req: Request, res: Response) {
-  openDb().then(db => {
-    db.all('SELECT * FROM pessoa')
-      .then(pessoas => res.json(pessoas))
+// _req é um parametro que não está sendo utilizado
+export async function selectPessoas(_req: Request, res: Response): Promise<void> {
+  return openDb().then(db => {
+    db.all('SELECT * FROM Pessoa')
+      .then(pessoas => res.json(pessoas));
   });
 };
 
-export async function selectPessoaID(req: Request, res: Response) {
-  let id = req.body.id;
-  openDb().then(db => {
-    db.get('SELECT * FROM pessoa WHERE id=?', [id])
-      .then(pessoa => res.json(pessoa));
-  });
+export async function selectPessoaID(req: Request, res: Response): Promise<void> {
+  let id = +req.params?.id;
+  // req.params
+  // req.body
+
+  const db = await openDb()
+  const pessoa = await db.get('SELECT * FROM Pessoa WHERE id=?', [id]);
+
+  if (!pessoa) res.status(404).json({ error: `Pessoa com id ${id} não foi encontrada` });
+  res.status(200).json(pessoa);
 };
 
-export async function insertPessoa(req: Request, res: Response) {
+export async function insertPessoa(req: Request, res: Response): Promise<void> {
   let pessoa = req.body;
+
   openDb().then(db => {
-    db.run('INSERT INTO Pessoa (name, idade) VALUES (?,?)', [pessoa.nome, pessoa.idade]);
+    db.run('INSERT INTO Pessoa (nome, idade) VALUES (?,?)', [pessoa.nome, pessoa.idade]);
   });
   res.json({
     "statusCode": 200
-  })
+  });
 };
 
-export async function updatePessoa(req: Request, res: Response) {
-  let pessoa = req.body;
+export async function updatePessoa(req: Request, res: Response): Promise<void> {
+  let pessoa = req.body.pessoa;
+
   openDb().then(db => {
     db.run('UPDATE Pessoa SET nome=? idade=? WHERE id=? ', [pessoa.nome, pessoa.idade, pessoa.id]);
   });
   res.json({
     "statusCode": 200
-  })
+  });
 };
 
-export async function deletePessoa(req: Request, res: Response) {
+export async function deletePessoa(req: Request, res: Response): Promise<void> {
   let id = req.body.id;
-  openDb().then(db => {
-    db.get('DELETE FROM pessoa WHERE id=?', [id])
-      .then(res => res)
-  });
+
+  const db = await openDb();
+  await db.get('DELETE FROM pessoa WHERE id=?', [id]);
   res.json({
     "statusCode": 200
-  })
+  });
 };
