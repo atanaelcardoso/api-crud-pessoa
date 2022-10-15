ecfd64202fd1eff650d08556485837d13ab6e6c4
Jonathan Cardoso <jonathan99mour@gmail.com>
2022-10-15 20:22:12 -0300

    Feat: Removendo arquivos e refatorando arquivo app com validacoes

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-import router from './routes/index'
+import router from './routes'
+
 app.use(router);
 
-// insonia app que ele usa
-// cors forma de seguransa , npm i cors -s, npm i https -s,npm i fs -s
-//estatus de seguranca 'https://pt.rakko.tools/tools/46/' dendro dele ele fez as pasta cors, https ,fs
-app.listen(3000, () => console.log("Api Rodando."))
+// CORS forma de segurança, npm i cors -s, npm i https -s,npm i fs -s
+// e status de seguranca 'https://pt.rakko.tools/tools/46/' dendro dele ele fez as pasta cors, https ,fs
+app.listen(3000, () => console.log("Api Rodando... porta 3000"))
 
-https.createServer({
-  cert: fs.readFileSync('src/SSL/code.crt'),
-  key: fs.readFileSync('src/SSL/code.key')
-}, app).listen(3001, () => console.log("Rodando em https"));
+if (process.env.NODE_ENV !== 'development') {
+    https.createServer({
+      cert: fs.readFileSync('src/SSL/code.crt'),
+      key: fs.readFileSync('src/SSL/code.key')
+    }, app).listen(3001, () => console.log("Rodando em https"));
+}
